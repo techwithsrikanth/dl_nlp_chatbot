8a11985050f055bc356d958ee39603e7624ad57f
Srikanthan <srikanthprakash072003@gmail.com>
2023-09-23 20:12:45 +0530

    first commit

--- python-deep-learning-chatbot/chatbot.py
+++ python-deep-learning-chatbot/chatbot.py
@@ -48,18 +48,17 @@ pickle.dump(words,open('words.pkl','wb'))
 pickle.dump(classes,open('classes.pkl','wb'))
 
 # initializing training data
+# initializing training data
 training = []
 output_empty = [0] * len(classes)
+
 for doc in documents:
-
     bag = []
-
     pattern_words = doc[0]
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
 
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-
 
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
@@ -67,10 +66,10 @@ for doc in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+
+# Separate the bag and output_row arrays
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
 print("Training data created")
 
 
@@ -84,10 +83,10 @@ model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-#fitting and saving the model
+# fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
--- python-deep-learning-chatbot/processor.py
+++ python-deep-learning-chatbot/processor.py
@@ -21,26 +21,21 @@ def clean_up_sentence(sentence):
 # return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
 def bow(sentence, words, show_details=True):
-    # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
     bag = [0]*len(words)
     for s in sentence_words:
         for i,w in enumerate(words):
             if w == s:
-                # assign 1 if current word is in the vocabulary position
                 bag[i] = 1
                 if show_details:
                     print ("found in bag: %s" % w)
     return(np.array(bag))
 
 def predict_class(sentence, model):
-    # filter out predictions below a threshold
     p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
